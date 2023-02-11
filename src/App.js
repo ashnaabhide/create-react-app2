50482067f25c598a68831465d796d9d699f6b63d
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-02-11 16:50:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+//var, const, let - all ways to define variables 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const - cannot modify once declared
+//let - can modify in certain circumstances
+import './App.css';
+function App(){
+
+
+const details = document.querySelector('summary');
+
+//toggle stats
+document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
+  //console.log(details.parentNode);
+  if(details.parentNode.getAttribute('open')){
+    details.parentNode.removeAttribute('open');
+  }
+  else{
+    details.parentNode.setAttribute('open', 'open');
+
+  }
+});
+
+//duplicate
+document.querySelector('.duplicate').addEventListener('click', (e) => {
+  
+  //could also name const item-clone
+  const itemClone = document.querySelector('.wrapper').cloneNode(true);
+  document.body.appendChild(itemClone);
+
+});
+
+/*
+document.querySelector('.wrapper').addEventListener('mouseenter', (e) => {
+    alert('haha');
+});
+*/
+
+//delete
+document.querySelector('button[data-delete-btn]').addEventListener('click', function(e) {
+  let wantsTo = confirm("are you sure?");
+    //if press 'ok' and last card isn't the OG card, then do it
+
+  if (wantsTo) {
+    if (document.querySelector('.wrapper:last-child') !== document.querySelector('.wrapper')) {
+      document.querySelector('.wrapper:last-child').remove();    
+    }
+    else {
+      alert("borzoi is forever!");
+    }
+  }
+});
+
+
+
+
+//modify heading
+//if you use an id instead of a class, it would be document.querySelector('#modify');
+document.querySelector('.modify').addEventListener('click', (e) => {  
+  let name = prompt('rename the borzoi!');
+  if(name) {
+    document.querySelector('.wrapper').querySelector('h3').innerText = name;
+  }
+});
+
+
 }
 
 export default App;
